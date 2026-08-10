48a1d95f48d9d0ec1016b6386274ac8f2f6db6ec
maggie_e_matthias <magdolinemad2007@gmail.com>
2026-08-10 17:59:28 +0300

    chore: setup main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+require('electron-reload')(__dirname)
+const {app, BrowserWindow} = require('electron');
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 320, 
+        height: 420, 
+        resizable: false,
+        maximizable: false, 
+        fullscreen: false, 
+        center: true, 
+        alwaysOnTop: true, 
+        webPreferences: {
+            nodeIntegration: true, 
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
